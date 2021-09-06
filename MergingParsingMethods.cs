176e6901d7f4259aad4e63d889f1917aaf34509a
Dawid Płocki <dawid.plocki@gmail.com>
2021-09-06 21:37:09 +0200

    Implement the merging of parsing methods

--- MergingParsingMethods.cs
+++ MergingParsingMethods.cs
@@ -12,13 +12,19 @@ namespace VagabondCsv
             using(var reader = new StreamReader("csvfiles/simple_with_additional_columns.csv"))
             using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
+                csv.Read();
+                csv.ReadHeader();
+
                 while(csv.Read())
                 {
                     var foo = csv.GetRecord<Foo>();
 
                     foo.PrintContent();
-                    Console.Write("Column count: ");
-                    Console.WriteLine(csv.ColumnCount.ToString());
+                    Console.WriteLine($"Column count {csv.Parser.Count}/{csv.HeaderRecord.Length}");
+
+                    for(var index = csv.HeaderRecord.Length; index < csv.Parser.Count; index++) {
+                        Console.WriteLine($"\t{csv.GetField(index).Trim()}");
+                    }
                 }
             }
         }
